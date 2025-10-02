bcffaca35e1a27969d27b02b421c12607c590151
Terekhov Artur <terehov.rtr@gmail.com>
2025-10-02 15:03:14 +0300

    added searchTest

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "lesson2-git"
---
+++ src/test/java/GuruTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class GuruTest {
+
+    @Test
+    void searchTest() {
+        System.out.println("Hello, friend!");
+    }
+
+}
